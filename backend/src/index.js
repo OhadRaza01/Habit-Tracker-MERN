3aaa3e384600559ebc8146c79dd0bd7174c2e3f8
OhadRaza01 <ohadraza345@gmail.com>
2026-08-17 18:27:25 +0500

    chore: setup backend application

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+import { app } from "./app.js";
+import "dotenv/config"
+import connectDB from "./db/index.js";
+
+const port = process.env.PORT || 3000
+
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`app is listening at port ${port}`)
+        })
+    })
+    .catch((err) => {
+        console.log("MongoDB connection failed !!", err)
+    })
