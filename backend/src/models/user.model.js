1d826efeafedbeba8379808ad0db88713cc28a3d
OhadRaza01 <ohadraza345@gmail.com>
2026-08-17 22:00:58 +0500

    feat: add user authentication model

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,78 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken"
+import "dotenv/config"
+import bcrypt from "bcrypt"
+
+const userSchema = new mongoose.Schema({
+
+    fullName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+    },
+    refreshToken: {
+        type: String,
+    }
+
+}, { timestamps: true })
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.pre("save", function () {
+
+    if (!this.isModified("password")) return;
+
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+
+export const User = mongoose.model("User", userSchema)
