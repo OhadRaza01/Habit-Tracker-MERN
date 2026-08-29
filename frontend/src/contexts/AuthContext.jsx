07e8b5da9d1a097b202ca5851d962243901ec3cb
OhadRaza01 <ohadraza345@gmail.com>
2026-08-29 17:37:18 +0500

    feat: enhance AuthContext to include loading state during user authentication

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,22 +1,38 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
 
-
-const AuthContext = createContext()
+const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const [user, setUser] = useState(null)
+    useEffect(() => {
+        const getCurrentUser = async () => {
+            try {
+                const response = await axios.get(
+                    "http://localhost:8000/api/v1/users/me",
+                    {
+                        withCredentials: true,
+                    }
+                );
+
+                setUser(response.data.data);
+            } catch (error) {
+                setUser(null);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        getCurrentUser();
+    }, []);
 
     return (
-        <AuthContext.Provider value={{ user, setUser }}>
+        <AuthContext.Provider value={{ user, setUser, loading }}>
             {children}
         </AuthContext.Provider>
     );
+};
 
-}
-
-{/*custom hook*/ }
-
-export const useAuth = () => {
-    return useContext(AuthContext)
-}
+export const useAuth = () => useContext(AuthContext);
